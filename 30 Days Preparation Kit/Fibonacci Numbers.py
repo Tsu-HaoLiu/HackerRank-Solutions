a0e2c6e147c25256ace2c86a3394979a3033af5a
Tsu-HaoLiu <96331813+Tsu-HaoLiu@users.noreply.github.com>
2023-07-12 15:20:34 -0700

    add multiple solutions

--- 30 Days Preparation Kit/Fibonacci Numbers.py
+++ 30 Days Preparation Kit/Fibonacci Numbers.py
@@ -4,3 +4,16 @@ def fibonacci(n):
     if n <= 1:
         return n
     return fibonacci(n-1) + fibonacci(n-2)
+
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+
+    prev = 0
+    curr = 1
+    
+    for _ in range(2, n + 1):
+        prev, curr = curr, prev + curr
+    
+    return curr
